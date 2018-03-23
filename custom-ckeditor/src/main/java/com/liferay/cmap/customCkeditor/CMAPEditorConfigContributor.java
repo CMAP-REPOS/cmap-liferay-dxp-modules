1417ba3857412437134bf1829875f1cb76cc73aa
Clark Nelson <mail@clarknelson.com>
2018-03-23 11:05:51 -0500

    added custom-ckeditor module start

---
+++ custom-ckeditor/src/main/java/com/liferay/cmap/customCkeditor/CMAPEditorConfigContributor.java
@@ -0,0 +1,45 @@
+
+package com.liferay.cmap.customCkeditor;
+
+import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
+import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import javax.portlet.Portlet;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+    property = {
+      "editor.config.key=cmapEditor",
+      "editor.name=ckeditor",
+      "javax.portlet.name=com_liferay_blogs_web_portlet_BlogsPortlet",
+      "service.ranking:Integer=100"
+    },
+
+    service = EditorConfigContributor.class
+)
+
+
+
+public class CMAPEditorConfigContributor extends BaseEditorConfigContributor {
+
+  @Override
+  public void populateConfigJSONObject( JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes, ThemeDisplay themeDisplay, RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+    System.out.println(jsonObject.toString());
+    JSONObject toolbars = jsonObject.getJSONObject("toolbars");
+
+    if (toolbars != null) {
+      JSONObject toolbarAdd = toolbars.getJSONObject("add");
+      if (toolbarAdd != null) {
+        JSONArray addButtons = toolbarAdd.getJSONArray("buttons");
+        addButtons.put("camera");
+      }
+    }
+
+  }
+}
