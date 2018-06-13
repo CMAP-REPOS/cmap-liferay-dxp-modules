db6b6e958db8e09d17021a48a1711942b2fa1b95
Ankur Goyal <ankur.goyal@workstate.com>
2018-06-13 16:30:05 -0400

    refactore the code.

---
+++ contact-manager-app/src/main/java/contact/manager/app/constants/CrmContactFieldKeys.java
@@ -0,0 +1,19 @@
+package contact.manager.app.constants;
+
+public class CrmContactFieldKeys {
+	
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String ORGANIZATION = "organization";
+	public static final String JOB_TITLE = "jonTitle";
+	public static final String PRIMARY_ADDRESS_1 = "primaryAddress1";
+	public static final String PRIMARY_ADDRESS_2 = "primaryAddress2";
+	public static final String PRIMARY_ADDRESS_CITY = "primaryAddressCity";
+	public static final String PRIMARY_ADDRESS_ZIP = "primaryAddressZip";
+	public static final String PRIMARY_ADDRESS_COUNTY = "primaryAddressCounty";
+	public static final String PRIMARY_PHONE = "primaryPhone";
+	public static final String PRIMARY_CELL = "primaryCell";
+	public static final String PRIMARY_EMAIL_ADDRESS = "primaryEmailAddress";
+	public static final String MODIFIED_DATE = "modifiedDate";
+	
+}
