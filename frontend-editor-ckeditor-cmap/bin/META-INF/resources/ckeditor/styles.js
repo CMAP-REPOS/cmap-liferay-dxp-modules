dc6bea5dcc58570d21a62110e26f5f403773fdf6
Clark Nelson <mail@clarknelson.com>
2018-07-26 10:42:22 -0500

    Add pseudo headline elements to apply style not structure

--- frontend-editor-ckeditor-cmap/bin/META-INF/resources/ckeditor/styles.js
+++ frontend-editor-ckeditor-cmap/bin/META-INF/resources/ckeditor/styles.js
@@ -7,5 +7,12 @@ if(Object.keys(CKEDITOR.stylesSet.registered).includes('cmap_styles')){
     { name: 'Large Paragraph', element: 'p', attributes: { 'class': 'presna-large' } },
     { name: 'Button', element: 'a', attributes: { 'href': '', 'class': 'button' } },
     { name: 'Alt Color', element: 'span', attributes: { 'class': 'alt-color' } },
+    { name: 'Pseudo Headline 6', element: 'div', attributes: { 'class': 'whitney-small bold' } },
+    { name: 'Pseudo Headline 5', element: 'div', attributes: { 'class': 'whitney-normal bold' } },
+    { name: 'Pseudo Headline 4', element: 'div', attributes: { 'class': 'whitney-middle bold' } },
+    { name: 'Pseudo Headline 3', element: 'div', attributes: { 'class': 'whitney-semi-large bold' } },
+    { name: 'Pseudo Headline 2', element: 'div', attributes: { 'class': 'whitney-large bold' } },
+    { name: 'Pseudo Headline 1', element: 'div', attributes: { 'class': 'whitney-huge bold' } },
+    { name: 'Pseudo Jumbo Headline', element: 'div', attributes: { 'class': 'whitney-jumbo bold' } },
 	]);
 }
