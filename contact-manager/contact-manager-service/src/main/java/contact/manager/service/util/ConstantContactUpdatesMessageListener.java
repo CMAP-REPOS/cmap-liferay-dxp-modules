588d29ddfc40bf1718eb4fddf9e7dde0310ee491
Xavier Muniz <xavier.muniz@base22.com>
2018-11-19 14:52:33 -0600

    Cleans up ContstantContactUpdatesMessageListener

--- contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/ConstantContactUpdatesMessageListener.java
+++ contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/ConstantContactUpdatesMessageListener.java
@@ -3,17 +3,43 @@ package contact.manager.service.util;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.module.framework.ModuleServiceLifecycle;
+import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
+import com.liferay.portal.kernel.scheduler.TriggerFactory;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
-		property = {"cron.expression=0 0 2 * * ?"}, // Will run every day at 2am
+		property = {"cron.expression=0 0 8 * * ?"}, // Will run every day at 2am. To run it every hour use: '0 0 * * * ?'. For every 5 minutes, use:'0 */5 * * * ?'. 
 		immediate = true,
 		service = ConstantContactUpdatesMessageListener.class )
 public class ConstantContactUpdatesMessageListener
 extends ContactManagerBaseMessageListener {
 	private static final Log _log = LogFactoryUtil.getLog(ConstantContactUpdatesMessageListener.class);
 
+	@Reference(unbind = "-")
+	public void setTriggerFactory(TriggerFactory triggerFactory) { _triggerFactory = triggerFactory; }
+
+	@Reference(unbind = "-")
+	public void setSchedulerEngineHelper(SchedulerEngineHelper schedulerEngineHelper) { _schedulerEngineHelper = schedulerEngineHelper; }
+
+	@Reference(target = ModuleServiceLifecycle.PORTAL_INITIALIZED, unbind = "-")
+	protected void setModuleServiceLifecycle( ModuleServiceLifecycle moduleServiceLifecycle ) { }
+
+	@Activate
+	@Override
+	protected void activate(Map<String,Object> properties) { super.activate(properties); }
+
+	@Deactivate
+	@Override
+	protected void deactivate() { super.deactivate(); }	
+
+	
 	
 	/**
 	 * doReceive: This method is called when the job is executed
@@ -23,9 +49,7 @@ extends ContactManagerBaseMessageListener {
 	 */
 	@Override
 	protected void doReceive(Message message) throws Exception {
-		if (_log.isInfoEnabled()) {
-			_log.info(">> doReceive ");
-		}
+		_log.info(">> doReceive ");
 		
 		
 		
@@ -33,8 +57,6 @@ extends ContactManagerBaseMessageListener {
 		
 		
 		
-		if (_log.isInfoEnabled()) {
-			_log.info("<< doReceive ");
-		}
+		_log.info("<< doReceive ");
 	}
 }
