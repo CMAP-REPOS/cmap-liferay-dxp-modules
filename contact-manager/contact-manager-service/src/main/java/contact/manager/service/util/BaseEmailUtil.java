1a1a7e89c34257ff672175ad48ca039c5d03d977
Xavier Muniz <xavier.muniz@base22.com>
2018-11-20 20:18:35 -0600

    Adds simple date formatter

--- contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/BaseEmailUtil.java
+++ contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/BaseEmailUtil.java
@@ -2,13 +2,23 @@ package contact.manager.service.util;
 
 import com.liferay.mail.kernel.model.MailMessage;
 import com.liferay.mail.kernel.service.MailServiceUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.text.SimpleDateFormat;
 
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
 public abstract class BaseEmailUtil {
+	private static final Log _log = LogFactoryUtil.getLog(BaseEmailUtil.class);
+	protected static final SimpleDateFormat _simpleDateFormatter = new SimpleDateFormat("MM/dd/yyyy");
 
-	static void sendEmail(String from, String to, String cc, String subject, String body, Boolean isHTMLFormat) throws AddressException {
+	
+	protected static void sendEmail(String from, String to, String cc, String subject, String body, Boolean isHTMLFormat) throws AddressException {
+		if (_log.isTraceEnabled()) {
+			_log.trace(">> sendEmail");
+		}
 		
 		InternetAddress fromAddress = new InternetAddress(from);
 		InternetAddress toAddress = new InternetAddress(to);
@@ -28,5 +38,9 @@ public abstract class BaseEmailUtil {
 		mailMessage.setHTMLFormat(isHTMLFormat);
 		
 		MailServiceUtil.sendEmail(mailMessage);
+
+		if (_log.isTraceEnabled()) {
+			_log.trace("<< sendEmail");
+		}
 	}
 }
