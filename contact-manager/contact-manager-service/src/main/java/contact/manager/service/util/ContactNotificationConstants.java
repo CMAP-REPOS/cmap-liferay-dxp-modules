22826e3ab02426cbe819a93974b17f718ed11143
Xavier Muniz <xavier.muniz@base22.com>
2019-06-12 17:49:44 -0500

    Adds environment constant and removes unused constants

--- contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/ContactNotificationConstants.java
+++ contact-manager/contact-manager-service/src/main/java/contact/manager/service/util/ContactNotificationConstants.java
@@ -5,10 +5,12 @@ public class ContactNotificationConstants {
 	public static final String UNSUBSCRIBED_VIP_NOTIFICATION_ENABLED = "contact.manager.service.util.UnsubscribedContactNotificationMessageListener.enabled";
 	public static final String UPDATES_NOTIFICATION_ENABLED = "contact.manager.service.util.UpdatedContactNotificationMessageListener.enabled";
 	
-	public static final String UNSUBSCRIBED_VIP_NOTIFICATION_CRON_EXPRESSION = "contact.manager.service.util.UnsubscribedContactNotificationMessageListener.cronexpression";
-	public static final String UPDATES_NOTIFICATION_CRON_EXPRESSION = "contact.manager.service.util.UpdatedContactNotificationMessageListener.cronexpression";
+	//public static final String UNSUBSCRIBED_VIP_NOTIFICATION_CRON_EXPRESSION = "contact.manager.service.util.UnsubscribedContactNotificationMessageListener.cronexpression";
+	//public static final String UPDATES_NOTIFICATION_CRON_EXPRESSION = "contact.manager.service.util.UpdatedContactNotificationMessageListener.cronexpression";
 
 	public static final String EMAIL_FROM_ADDRESS = "contact.manager.service.util.BaseEmailUtil.from";
 	public static final String EMAIL_TO_ADDRESS = "contact.manager.service.util.BaseEmailUtil.to";
 	public static final String EMAIL_CC_ADDRESS = "contact.manager.service.util.BaseEmailUtil.cc";
+	
+	public static final String ENVIRONMENT_NAME = "contact.manager.service.util.EnvironmentName";
 }
